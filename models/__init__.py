1448b82263e9167913878981f70c923f015eae20
gitagenterprise-bit <gitagenterprise@gmail.com>
2026-08-09 12:42:05 +0530

    Create __init__.py

---
+++ models/__init__.py
@@ -0,0 +1,15 @@
+from models.user import User
+from models.customer import CustomerProfile
+from models.worker import WorkerProfile
+from models.category import Category
+from models.job import Job, JobImage
+
+
+__all__ = [
+    "User",
+    "CustomerProfile",
+    "WorkerProfile",
+    "Category",
+    "Job",
+    "JobImage"
+]
