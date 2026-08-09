94e772de7dcdba62d25fd61d9caf4eae3cf7da40
gitagenterprise-bit <gitagenterprise@gmail.com>
2026-08-09 12:54:42 +0530

    Update customer.py

--- routes/customer.py
+++ routes/customer.py
@@ -1,7 +1,23 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+
+from utils.decorators import role_required
 
 
 customer_bp = Blueprint(
     "customer",
     __name__
 )
+
+
+@customer_bp.route(
+    "/dashboard",
+    methods=["GET"]
+)
+@role_required("customer")
+def dashboard():
+
+    return jsonify({
+        "status": "success",
+        "message": "Welcome to Customer Dashboard",
+        "role": "customer"
+    })
