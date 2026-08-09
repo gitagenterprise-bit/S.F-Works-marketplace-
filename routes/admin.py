80c4156f01a03db76e1e2b09603ee2b41fba250b
gitagenterprise-bit <gitagenterprise@gmail.com>
2026-08-09 12:55:50 +0530

    Update admin.py

--- routes/admin.py
+++ routes/admin.py
@@ -1,7 +1,23 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+
+from utils.decorators import role_required
 
 
 admin_bp = Blueprint(
     "admin",
     __name__
 )
+
+
+@admin_bp.route(
+    "/dashboard",
+    methods=["GET"]
+)
+@role_required("admin")
+def dashboard():
+
+    return jsonify({
+        "status": "success",
+        "message": "Welcome to Admin Dashboard",
+        "role": "admin"
+    })
