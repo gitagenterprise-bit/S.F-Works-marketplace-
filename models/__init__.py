2576b387ecf4421a28b4d02a9da0954db262368f
gitagenterprise-bit <gitagenterprise@gmail.com>
2026-08-09 20:08:48 +0530

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,8 @@
 from models.user import User
 from models.customer import CustomerProfile
 from models.worker import WorkerProfile
+from models.worker_skill import WorkerSkill
+from models.worker_portfolio import WorkerPortfolio
 from models.category import Category
 from models.job import Job, JobImage
 from models.job_application import JobApplication
@@ -10,6 +12,8 @@ __all__ = [
     "User",
     "CustomerProfile",
     "WorkerProfile",
+    "WorkerSkill",
+    "WorkerPortfolio",
     "Category",
     "Job",
     "JobImage",
