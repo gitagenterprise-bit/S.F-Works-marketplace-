005d07796979ddeee0382cfae56c00427104ee2b
gitagenterprise-bit <gitagenterprise@gmail.com>
2026-08-09 12:55:17 +0530

    Update worker.py

--- routes/worker.py
+++ routes/worker.py
@@ -1,7 +1,23 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+
+from utils.decorators import role_required
 
 
 worker_bp = Blueprint(
     "worker",
     __name__
 )
+
+
+@worker_bp.route(
+    "/dashboard",
+    methods=["GET"]
+)
+@role_required("worker")
+def dashboard():
+
+    return jsonify({
+        "status": "success",
+        "message": "Welcome to Worker Dashboard",
+        "role": "worker"
+    })
