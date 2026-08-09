4366504e6d90b28df95ae25b89cb9530c0af5245
gitagenterprise-bit <gitagenterprise@gmail.com>
2026-08-09 12:47:44 +0530

    Update auth.py

--- routes/auth.py
+++ routes/auth.py
@@ -1,4 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+
+from flask_jwt_extended import (
+    create_access_token,
+    create_refresh_token,
+    jwt_required,
+    get_jwt_identity
+)
+
+from sqlalchemy import or_
+
+from extensions import db
+from models.user import User
+from models.customer import CustomerProfile
+from models.worker import WorkerProfile
 
 
 auth_bp = Blueprint(
