b8926d829e395bfa018f6d1cbc064ce6037c47bf
gitagenterprise-bit <gitagenterprise@gmail.com>
2026-08-09 12:56:23 +0530

    Update jobs.py

--- routes/jobs.py
+++ routes/jobs.py
@@ -1,7 +1,19 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
 
 jobs_bp = Blueprint(
     "jobs",
     __name__
 )
+
+
+@jobs_bp.route(
+    "/",
+    methods=["GET"]
+)
+def jobs():
+
+    return jsonify({
+        "status": "success",
+        "message": "Public jobs endpoint"
+    })
