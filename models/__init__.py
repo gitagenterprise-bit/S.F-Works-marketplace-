79cdebda0c58b30168299c95135dcf87ef113e8c
gitagenterprise-bit <gitagenterprise@gmail.com>
2026-08-23 09:27:04 +0530

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -36,4 +36,4 @@ from models.audit_log import (
     AuditLog
 )
 from models.agent import AgentProfile
-from models.approval_record import ApprovalRecord
+from models.approval import ApprovalRecord
