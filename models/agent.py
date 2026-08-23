5343ef4fd551d0cee8d7f8877e255b958bb5fdcf
gitagenterprise-bit <gitagenterprise@gmail.com>
2026-08-23 10:57:02 +0530

    Update agent.py

--- models/agent.py
+++ models/agent.py
@@ -379,12 +379,7 @@ class AgentPermission(db.Model):
         foreign_keys=[granted_by]
     )
 
-    jobs = db.relationship(
-        "Job",
-        foreign_keys="Job.agent_id",
-        back_populates="agent",
-        lazy="selectin"
-    )
+    
 
     __table_args__ = (
         db.UniqueConstraint(
